563808d4ca519c1ba1c13f1a476e49ecea5293ce
hsibayan <hannahsibayan@gmail.com>
2016-03-27 23:01:27 +0800

    First Commit grrr

---
+++ Assets/movingBackground.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class movingBackground : MonoBehaviour {
+
+    public Material bgA, bgB, bgC;
+
+    private MeshRenderer renderer;
+
+    public float scrollSpeed;
+    public float tileSizeZ;
+    
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        startPosition = transform.position;
+
+        System.Random rand = new System.Random();
+        int randomBG = rand.Next(3);
+
+        switch (randomBG)
+        {
+            case 0: renderer.material = bgA; break;
+            case 1: renderer.material = bgB; break;
+            case 2: renderer.material = bgC; break;
+        }
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
+}
